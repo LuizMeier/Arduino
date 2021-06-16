f3a63b38f220f166a914933a6922cfcac3b7d6a7
LuizMeier <lfmmeier@gmail.com>
2021-06-16 09:25:49 -0300

    Primeiro

---
+++ Ambilight/Escritorio.cpp
@@ -0,0 +1,72 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 2	 // input pin Neopixel is attached to
+
+#define NUMPIXELS      4 // number of neopixels in strip
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 100; // timing delay in milliseconds
+
+int redColor = 0;
+int greenColor = 0;
+int blueColor = 0;
+
+int buttonState = 0;
+int buttonCount = 0;
+
+
+void setup() {
+  // Initialize the NeoPixel library.
+  pixels.begin();
+  
+  // Setup button
+  pinMode (1, INPUT);
+  
+}
+
+
+void loop() {
+	
+	buttonState = digitalRead(1);
+	
+	if (buttonState == HIGH) {
+	  buttonCount = buttonCount + 1;
+	}
+	
+	if (buttonCount == 1) {
+		redColor = 0;
+		greenColor = 255;
+		blueColor = 0;
+	}
+	
+	else if (buttonCount == 2) {
+		redColor = 0;
+		greenColor = 0;
+		blueColor = 255;
+	}
+	
+	else if (buttonCount == 3) {
+		redColor = 255;
+		greenColor = 0;
+		blueColor = 0;
+	}
+	
+	else {
+	  buttonCount = 0;
+	}
+
+  for (int i=0; i < NUMPIXELS; i++) {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(redColor, greenColor, blueColor));
+
+    // This sends the updated pixel color to the hardware.
+    pixels.show();
+
+    // Delay for a period of time (in milliseconds).
+    delay(delayval);
+  }
+
+
+
+}
